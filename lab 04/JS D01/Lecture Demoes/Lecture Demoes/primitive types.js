9ed30e8491de974cf6025cb72f811734eda4ec2e
Mahmoud Ghanem <moghanem345@gmail.com>
2023-08-23 12:12:26 +0300

    add lab 04

---
+++ lab 04/JS D01/Lecture Demoes/Lecture Demoes/primitive types.js
@@ -0,0 +1,21 @@
+﻿//alert("welcome from External ")
+
+//console.log(`str= ${str} and type= ${typeof str}`)
+
+//console.log(`num = ${num}type= ${typeof num}`)
+//console.log(`str+str = ${str + str}`)
+//console.log(`num+num = ${num + num}`)
+//console.log(`num+str = ${num + str}`)
+
+//console.log(`x => type= ${typeof x}`)
+//console.log(`y => type= ${typeof y}`)
+
+//2 == 2 // true
+//2=="2" // true
+
+2 === 2 // true 
+2 === "2" // false
+
+x == y // true
+x === y // false
+
